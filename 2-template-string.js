74323f9b188cd845bbbad02035bcc4b63c4d09f0
idarifu <idarifurrahman@gmail>
2022-02-23 10:41:38 +0600

    first commit

---
+++ 2-template-string.js
@@ -0,0 +1,20 @@
+const multiLineNew = `This is first line.
+This is second line.
+This ids third line.
+This is fourth line. 
+This is fifth line.`
+
+// console.log(multiLineNew)
+const friends = ['Abul', 'Kabul', 'Sabul', 'Babul']
+const count = 5;
+const newest = `This is my friend number-${friends[2]}`;
+
+console.log(newest)
+
+const firstName = 'Arifur';
+const secondName = 'Rahman'
+const fullName = `This Person's name is: ${firstName} ${secondName}`;
+console.log(fullName)
+
+const myMoney = `Rahim has more than ${friends.length + 200000 * 500} taka.`
+console.log(myMoney);
---
+++ 3-arrow-function.js
@@ -0,0 +1,110 @@
+// function add(num1, num2) {
+//     return num1 + num2;
+
+// }
+
+// const sum = add(12, 12);
+
+// // console.log(sum)
+
+// const add2 = function add2(num1, num2) {
+//     return num1 + num2;
+// }
+
+// const sum2 = add2(12, 12)
+
+// // console.log(sum, sum2)
+
+// const add3 = function (num1, mum2) {
+//     return num1 + num2;
+// }
+
+// const sum3 = add(12, 12)
+// // console.log(sum, sum2, sum3)
+
+
+// const add4 = (num1, num2) => num1 + num2;
+
+// const sum4 = add(12, 12);
+
+// // console.log(sum4)
+
+
+// const dividing = (number) => number / 5;
+// const ans1 = dividing(20);
+// // console.log(ans1)
+
+
+// const addMultiply = (x, y) => (x + 2) * (y + 2);
+// const ans2 = addMultiply(10, 10);
+
+// // console.log(ans2)
+
+// // 3.c arrow function with three parameters. will multiply all three parameters:
+
+// const multiply = (a, b, c) => a * b * c;
+// const ans3 = multiply(10, 10, 10);
+
+// // console.log(ans3)
+
+// // ৩.৪ এইবার দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন নিবে। ওই arrow ফাংশনটা হবে অনেকগুলা লাইনের। সেখানে প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো।
+
+// const doMath = (x, y) => {
+//     const mathOne = x + 2;
+//     const mathTwo = y + 2;
+//     const mathResult = (x + 2) * (y + 2)
+//     return mathResult
+// }
+
+// const myInput = doMath(10, 10);
+
+// console.log(myInput)
+
+// অনেকগুলা সংখ্যার একটা array হবে। তারপর তোমার কাজ হবে array এর উপরে map ইউজ করে। প্রত্যেকটা উপাদানকে ৫ দিয়ে গুন্ করে গুনফল আরেকটা array হিসেবে রাখবে। পুরা কাজটা এক লাইনে হবে।
+
+
+// Older Version without using map
+
+// const numbers = [4, 6, 8, 10, 12, 24, 46, 96, 192]
+// const output = [];
+// for (const number of numbers) {
+//     const result = number * 5;
+//     output.push(result)
+// }
+
+// console.log(output);
+
+
+
+// Using arrow ......
+
+
+// const numbers = [4, 6, 8, 10, 12, 24, 46, 96, 192]
+
+// const fiveXIt = number => number * 5;
+// const output = [];
+
+// for (const number of numbers) {
+//     const result = fiveXIt(number);
+//     output.push(result)
+
+// }
+// console.log(output);
+
+// Using map.....
+
+const numbers = [4, 6, 8, 10, 12, 24, 46, 96, 19]
+
+const output = numbers.map(number => number * 5)
+
+console.log(output)
+
+
+
+
+
+
+
+
+
+
